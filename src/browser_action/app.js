be0f1cfbfa029f7ff1674758f6f09d88af71dfdc
hwclass <harderworking.class@gmail.com>
2014-06-30 23:30:18 +0300

    Added comment in app.js file.

--- src/browser_action/app.js
+++ src/browser_action/app.js
@@ -1,16 +1,4 @@
-Vue.component('product-list', {
-		template :  '<a class="product-link" href="{{url_key}}" target="_blank">' + 
-								'<img src="{{images}}" alt="{{brand_name}} {{name}}">' +
-								'<div class="price">' + 
-								'<p class="brand-name">{{brand_name}}</p>' +
-								'<p class="brand-detail">{{short_description}}</p>' + 
-								'<p class="price-detail-old">{{actual_price}} TL</p>' +
-								'<p class="price-detail">{{sale_price}}</p>' +
-								'</div>' +
-								'<div class="product-overlay hidden"></div>' +
-								'</a>'
-})
-
+/*Vue Main App File for Product List :: productListApp*/
 var productListApp = new Vue({
 	
 	el: '#product_list',
